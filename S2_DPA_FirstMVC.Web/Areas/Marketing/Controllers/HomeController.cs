e738d733e0d58f9dc0713c3dfd82507ed3030f52
falvino <19100188@ue.edu.pe>
2021-09-12 13:04:37 -0500

    ok 3

--- S2_DPA_FirstMVC.Web/Areas/Marketing/Controllers/HomeController.cs
+++ S2_DPA_FirstMVC.Web/Areas/Marketing/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using S2_DPA_FirstMVC.Web.Areas.Marketing.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,6 +15,16 @@ namespace S2_DPA_FirstMVC.Web.Areas.Marketing.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+
+
+        public IEnumerable<Product> GetJsonProducts()
+        {
+            var folder = Path.Combine(Directory.GetCurrentDirectory(), "Areas\\Marketing\\Data\\Products.json");
+            var json = System.IO.File.ReadAllText(folder);
+            var products = JsonConvert.DeserializeObject<IEnumerable<Product>>(json);
+            return products;
         }
     }
 }
